34f9addbd2f74eb102eeedf55791b125b8c187b4
KUBOTA YASUHIRO <cooboo@M4-MBA.local>
2026-01-14 14:53:38 +0900

    影調整

--- client/src/components/EstimateResult.tsx
+++ client/src/components/EstimateResult.tsx
@@ -381,7 +381,7 @@ export function EstimateResult() {
           type="button"
           variant="outline"
           onClick={handleGoBack}
-          className="h-14 flex-1 border-[3px] border-black rounded-xl font-bold text-base shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] hover:shadow-[1px_1px_0px_0px_rgba(0,0,0,1)] hover:translate-x-[2px] hover:translate-y-[2px] transition-all bg-white"
+          className="h-14 flex-1 border-[3px] border-black rounded-xl font-bold text-base shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:shadow-[1px_1px_0px_0px_rgba(0,0,0,1)] hover:translate-x-[2px] hover:translate-y-[2px] transition-all bg-white"
         >
           <ArrowLeft className="w-5 h-5 mr-2" />
           条件を変更
