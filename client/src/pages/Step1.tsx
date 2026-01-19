04e2a159a59a69486f723b98e32b7aeaafb3265f
KUBOTA YASUHIRO <cooboo@M4-MBA.local>
2026-01-20 07:51:41 +0900

    Step1のデザイン修正

--- client/src/pages/Step1.tsx
+++ client/src/pages/Step1.tsx
@@ -35,7 +35,7 @@ export default function Step1() {
         <div className="max-w-2xl mx-auto">
           <AddressForm />
         </div>
-        
+
         {/* フッター装飾 */}
         <div className="text-center mt-12 text-gray-400">
           <Sparkles className="inline-block w-5 h-5 mr-2" />
